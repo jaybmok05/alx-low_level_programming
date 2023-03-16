a3072be52fbd1a8aac3c8d7fd6256259b787d060
jabulane <jabumokoena85@gmail.com>
2023-03-16 11:08:11 -0700

    adding 4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ *main - this main function prints using puts().
+ *Return: return 0 to end the function.
+ */
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
