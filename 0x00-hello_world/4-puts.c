9f8db22a149c7e7c0e77c43dca33bb0d1ae951c1
jabulane <jabumokoena85@gmail.com>
2023-03-16 05:16:45 -0700

    corrections

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,11 @@
 #include <stdio.h>
 
+/*Main - this function prints using puts() 
+ *Return - it return 0
+ */
+
 int main(void)
 {
 	puts("Programming is like building a multilingual puzzle");
-	(return) 0;
+	return (0);
 };
