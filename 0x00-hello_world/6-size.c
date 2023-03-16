34bb8a5a3ddc5171d3a81471d2577147f88dce44
jabulane <jabumokoena85@gmail.com>
2023-03-16 11:47:47 -0700

    adding 6-size.c

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - using prinf to print sizeof data types
+ * Return: this returns 0 always
+ */
+
+int main(void)
+{
+	printf("\%Size of a char:\n", sizeof(char));
+	printf("\%Size of an int:\n", sizeof(int));
+	printf("\%Size of a long int:\n", sizeof(long int));
+	printf("\%Size of a long long int:\n", sizeof(long long int));
+	printf("\%Size of a float:\n", sizeof(float));
+	return (0);
+}
