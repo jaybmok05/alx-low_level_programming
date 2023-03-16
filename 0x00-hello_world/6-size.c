a52baf35ba68cea29da30f787b6ec9a04bac91eb
jabulane <jabumokoena85@gmail.com>
2023-03-16 12:24:04 -0700

    updating file

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -7,10 +7,16 @@
 
 int main(void)
 {
-	printf("%\"Size of a char:\n", sizeof(char));
-	printf("%\"Size of an int:\n", sizeof(int));
-	printf("%\"Size of a long int:\n", sizeof(long int));
-	printf("%\"Size of a long long int:\n", sizeof(long long int));
-	printf("%\"Size of a float:\n", sizeof(float));
+	char fav_letter;
+	int age;
+	long int id;
+	long long int b;
+	float weight;
+
+	printf("Size of a char: %lu byte(s)\n", (unsigned long)sizeof(fav_letter));
+	printf("Size of an int: %lu byte(s)\n", (unsigned long)sizeof(age));
+	printf("Size of a long int: %lu byte(s)\n", (unsigned long)sizeof(id));
+	printf("Size of a long long int: %lu byte(s)\n", (unsigned long)sizeof(b));
+	printf("Size of a float: %lu byte(s)\n", (unsigned long)sizeof(weight));
 	return (0);
 }
