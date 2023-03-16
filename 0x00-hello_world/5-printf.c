c9b7c0bf2da66c5302f4301d13f4ce28dffc1fff
jabulane <jabumokoena85@gmail.com>
2023-03-16 11:18:14 -0700

    adding 5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - this function prints using printf()
+ * Return: return 0
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
