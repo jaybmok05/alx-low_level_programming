6e1ec4654c8aaebff4dd19269e06c6ab5bb9d8b3
jabulane <jabumokoena85@gmail.com>
2023-03-20 02:04:55 -0700

    Modifying 1-last_digit.c

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -1,49 +1,31 @@
 #include <stdlib.h>
 #include <time.h>
 #include <stdio.h>
-#include <string.h>
 
 /**
- * main - this is the starting point for function
- * Return: returns 0
+ * main - this is the entry point for the program
+ *
+ * Return: returns 0 for Successful exe
  */
 
 int main(void)
 {
 	int n;
-	char n_str[20];
-	char last_dg;
-	int n_num;
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	/* your code goes there */
-	sprintf(n_str, "%d", n);
-	last_dg = n_str[strlen(n_str) - 1];
-	n_num = last_dg - '0';
 
-	if (n_num < 6 && n_num != 0)
+	if ((n % 10) == 0)
 	{
-		if (n_num < 0)
-		{
-			n_num = -n_num;
-			printf("Last digit of %d is %d and is less than 6 and not 0\n", n, n_num);
-		}
-		else
-		{
-			printf("Last digit of %d is %d and is less than 6 and not 0\n", n, n_num);
-		}
+		printf("Last digit of %d is %d and is 0\n", n, n % 10);
+	}
+	else if ((n % 10) > 5)
+	{
+		printf("Last digit of %d is %d and is greater than 5\n", n, n % 10);
 	}
 	else
 	{
-		if (n_num > 5)
-		{
-			printf("Last digit of %d is %d and is greater than 5", n, n_num);
-		}
-		else if (n_num == 0)
-		{
-			printf("Last digit of %d is %d and is 0", n, n_num);
-		}
+		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, n % 10);
 	}
 	return (0);
 }
