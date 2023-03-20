835a2e47d17411f638ed21efbc01f5002469b79d
jabulane <jabumokoena85@gmail.com>
2023-03-20 10:08:41 -0700

    adding 0putchar and modifying 1010-quote

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,15 @@
+#include <main.h>
+
+/**
+ * main - this function prints
+ * alphabet in lowercase
+ *
+ * Return: returns 0 when successful
+ */
+
+int main(void)
+{
+	_putchar("_putchar");
+	_putchar("\n");
+	return (0);
+}
