0e4af479d102f1e59fef03d73def6de3c62b3c79
jabulane <jabumokoena85@gmail.com>
2023-03-22 01:28:25 -0700

    adding 3-islower.c

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * _islower - this checks if letter is
+ * lowercase
+ *
+ * Return: returns 1 if lower case, 0 if not.
+ *
+ * @c: this is the parameter.
+ */
+
+int _islower(int c)
+{
+	if (c == 'c')
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -5,6 +5,12 @@
  * _putchar - this function writes char
  * to the stdout
  *
+ * print_alphabet - this function prints alphabets from a to z
+ *
+ * print_alphabet_x10 - this function prints print_alphabet ten times
+ *
+ * _islower - this check for lowercase letters
+ *
  * Return: On success 1.
  * On error, -1 is returned, and errno is set appropriately.
  * @char: it is the data type for parameter.
@@ -13,5 +19,6 @@
 int _putchar(char);/*function prototype for putchar.c*/
 void print_alphabet(void);/*function prototype for 1-alphabet.c*/
 void print_alphabet_x10(void);/*function prototype for 2-print_alphabet_x10.c*/
+int _islower(int c);/*function prototype for 3-islower.c*/
 
 #endif
