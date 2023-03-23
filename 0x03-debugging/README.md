e16810598fd1ced3e89e25405a79689b4b764830
jabulane <jabumokoena85@gmail.com>
2023-03-23 04:10:22 -0700

    adding 0x03-debugging/

---
+++ 0x03-debugging/README.md
@@ -0,0 +1,2 @@
+0. Multiple mains - in this task the instruction is 
+test 0-positive_or_negative in case of 0.
---
+++ 0x03-debugging/main.h
@@ -0,0 +1,11 @@
+#ifndef MAIN_FILE_H
+#define MAIN_FILE_H
+
+/**
+ * main - tests 0-positive_or_negative function
+ * in case of 0.
+ * Return: returns 0 when successful.
+ */
+
+int main(void);/*function prototype for 0-positive_or_negative.c*/
+#endif
