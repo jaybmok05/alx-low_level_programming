931c6ccc222fa93df83965397eb3d240315fdb06
jabulane <jabumokoena85@gmail.com>
2023-03-27 11:15:58 -0700

    modifying 2 strlen program

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -8,12 +8,16 @@
 
 int _strlen(char *s)
 {
-	int length;
+	int length = 0;
 	int i;
 
 	for (i = 0; s[i] != '\0'; i++)
 	{
 		length = i + 1;
+		if ((s = ""))
+		{
+			length = 0;
+		}
 	}
 	return (length);
 }
