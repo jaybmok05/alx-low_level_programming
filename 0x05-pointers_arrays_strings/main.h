eb4131f4547b2795f919e09f63c1ff10778e7193
jabulane <jabumokoena85@gmail.com>
2023-03-27 06:15:05 -0700

    adding reset to 98 program

---
+++ 0x05-pointers_arrays_strings/main.h
@@ -0,0 +1,16 @@
+#ifndef MAIN_FILE_H
+#define MAIN_FILE_H
+
+/**
+ * _putchar - prints out a character
+ * to the stdout.
+ * reset_to_98 -  a function that takes a pointer to an int
+ * as parameter and updates the value it points to to 98.
+ * Return: returns the character.
+ * @c: the parameter.
+ */
+
+/*list of function prototypes*/
+int _putchar(char c);
+void reset_to_98(int *n);
+#endif
