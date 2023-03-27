7c952195364b63b42effadf417cc16eb64345ddc
jabulane <jabumokoena85@gmail.com>
2023-03-27 12:32:18 -0700

    adding 3-puts.c

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -8,6 +8,8 @@
  * as parameter and updates the value it points to to 98.
  * swap_int - a function that swaps the values of two integers.
  * _strlen - a function that returns the length of a string.
+ * _puts - a function that prints a string, followed
+ * by a new line, to stdout.
  * Return: returns the character.
  * @c: the parameter.
  */
@@ -17,4 +19,5 @@ int _putchar(char c);
 void reset_to_98(int *n);
 void swap_int(int *a, int *b);
 int _strlen(char *s);
+void _puts(char *str);
 #endif
