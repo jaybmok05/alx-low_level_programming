464a31362f31763417804bfe806f372d7d631e19
jabulane <jabumokoena85@gmail.com>
2023-03-27 08:35:15 -0700

    adding 2-strlen.c

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -7,6 +7,7 @@
  * reset_to_98 -  a function that takes a pointer to an int
  * as parameter and updates the value it points to to 98.
  * swap_int - a function that swaps the values of two integers.
+ * _strlen - a function that returns the length of a string.
  * Return: returns the character.
  * @c: the parameter.
  */
@@ -15,4 +16,5 @@
 int _putchar(char c);
 void reset_to_98(int *n);
 void swap_int(int *a, int *b);
+int _strlen(char *s);
 #endif
