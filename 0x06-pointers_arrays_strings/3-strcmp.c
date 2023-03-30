4c24d3aee6d94545eddd4902d582d5cf6f57a421
jabulane <jabumokoena85@gmail.com>
2023-03-30 04:45:02 -0700

    adding 3-strcmp.c

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _strcmp - this compares to
+ * strings.
+ * Return: the result of the compared
+ * strings
+ * @s1: 1st parameter
+ * @s2: 2nd parameter
+ */
+
+int _strcmp(char *s1, char *s2)
+{
+	return (s1 == s2);
+}
