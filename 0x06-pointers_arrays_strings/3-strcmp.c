4b180d34c27c8df8ca630742ca29a51e6af3127a
jabulane <jabumokoena85@gmail.com>
2023-03-30 06:16:55 -0700

    adding strcmp

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -11,5 +11,13 @@
 
 int _strcmp(char *s1, char *s2)
 {
-	return (s1 == s2);
+	int index;
+	char con;
+
+	for (index = 0 , con = 1; con; index++)
+	{
+		con = (s1[index] == s2[index] &&
+				s2[index] != '\0');
+	}
+	return (s1[index - 1] - s2[index - 1]);
 }
