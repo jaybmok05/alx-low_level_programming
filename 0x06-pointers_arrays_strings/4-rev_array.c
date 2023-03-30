2e20d0cd924f2971a5dee22e92474e8298387bdb
jabulane <jabumokoena85@gmail.com>
2023-03-30 00:58:14 -0700

    adding 0-strcat.c

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * reverse_array - this function
+ * reerves an array
+ * @a: 1st parameter.
+ * @n: second parameter.
+ */
+
+void reverse_array(int *a, int n)
+{
+	int k;
+	n = sizeof(a) / sizeof(a[0]);
+
+	for (k = n - 1; k >= 0; k++)
+	{}
+}
---
+++ 0x06-pointers_arrays_strings/main.h
@@ -0,0 +1,19 @@
+#ifndef MAIN_FILE_H
+#define MAIN_FILE_H
+
+/**
+ * _putchar - this prints out a character to
+ * the stdout
+ * Return: return 0 on success
+ * On error, -1 is returned, and
+ * errno is set appropriately.
+ * reverse_array - reverses an array.
+ * @c: the parameter which is the
+ * character.
+ */
+
+/*list of functions prototype*/
+int _putchar(char c);
+char *_strcat(char *dest, char *src);
+void reverse_array(int *a, int n);
+#endif
