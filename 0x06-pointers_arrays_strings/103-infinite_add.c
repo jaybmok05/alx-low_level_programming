5e490591dfc6c6352d71eb2e696a877d25c7bcfd
jabulane <jabumokoena85@gmail.com>
2023-03-31 14:23:47 -0700

    adding files

---
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * infinite_add -  a function that adds two numbers.
+ * @n1: 1st paramter
+ * @n2: second parameter
+ * @r: 3rd parameter
+ * @size_r: 4th parameter
+ * Return: returns 0 If the result can not be stored in r
+ * and returns a pointer to the result
+ */
+
+char *infinite_add(char *n1, char *n2, char *r, int size_r)
+{
+}
---
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+ * print_buffer -  a function that prints a buffer.
+ * @b: 1st parameter
+ * @size: 2nd parameter
+ */
+
+void print_buffer(char *b, int size)
+{
+}
