5e42de325b0a51411bdd61eb002105135880fe9c
jabulane <jabumokoena85@gmail.com>
2023-04-02 11:47:23 -0700

    modifying infinte add

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -12,4 +12,41 @@
 
 char *infinite_add(char *n1, char *n2, char *r, int size_r)
 {
+	int len_1 = 0, len_2 = 0, sum = 0, index = size_r - 2;
+
+	while (n1[len_1 + 1] != 0)
+	{
+		len_1++;
+	}
+	while (n2[len_2 + 1] != 0)
+	{
+		len_2++;
+	}
+	r[size_r - 1] = 0;
+
+	while (index >= 0 && (len_1 >= 0 || len_2 >= 0))
+	{
+		sum += (len_1 < 0 ? '0' : n1[len_1]) +
+				(len_2 < 0 ? '0' : n2[len_2]);
+		sum -= 2 * '0';
+		r[index] = sum % 10 + '0';
+		sum /= 10;
+		index--;
+		len_1--;
+		len_2--;
+	}
+	if ((index < len_1 || index < len_2) || (index < 0 && sum))
+	{
+		return (0);
+	}
+	if (sum != 0)
+	{
+		r[index] = sum + '0';
+	}
+	else
+	{
+		sum = 1;
+	}
+	index += sum ? 0 : 1;
+	return (r + index);
 }
