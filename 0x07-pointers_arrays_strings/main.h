e83c729c5a4542ef0d57d7bc36106005a3eccbfd
jabulane <jabumokoena85@gmail.com>
2023-04-04 18:29:21 -0700

    adding 7-print_chessboard

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -13,6 +13,8 @@ char *_memset(char *s, char b, unsigned int n);
 char *_memcpy(char *dest, char *src, unsigned int n);
 char *_strchr(char *str, char c);
 unsigned int _strspn(char *str, char *accept);
-char *_strpbrk(char *s, char *accept);
+char *_strpbrk(char *str, char *accept);
 char *_strstr(char *haystack, char *needle);
+void print_chessboard(char (*arr)[8]);
+void print_diagsums(int *arr, int size);
 #endif
