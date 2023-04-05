bc8ee66067475258188aba83f1aab331fa2ea274
jabulane <jabumokoena85@gmail.com>
2023-04-05 00:40:39 -0700

    modifying 5-strstr

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -11,16 +11,25 @@
 
 char *_strstr(char *haystack, char *needle)
 {
-	int index, index_1;
+	int index, index_1, in_hay;
 
+	/*loop over haystack*/
 	for (index = 0; haystack[index] != '\0'; index++)
 	{
+		in_hay = 1;
+		/*loop over needle*/
 		for (index_1 = 0; needle[index_1] != '\0'; index_1++)
 		{
-			if (haystack[index] == needle[index_1])
+			if (haystack[index + index_1] != needle[index_1])
 			{
-				return (needle + index);
+				in_hay = 0;
+				break;
 			}
+		}
+		switch (in_hay)
+		{
+		case 1:
+			return (haystack + index);
 		}
 	}
 	return (0);
