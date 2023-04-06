368748006f624dbb7f3927bd65c9a3698139d0ed
jabulane <jabumokoena85@gmail.com>
2023-04-06 02:58:34 -0700

    modifying

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -8,17 +8,15 @@
 
 int _strlen_recursion(char *str)
 {
-	int index = 0;
 	int len_str = 0;
 
-	if (str[index] != '\0')
-	{
-		len_str++;
-		_strlen_recursion(str + 1);
-	}
-	else if (str[index] == '\0')
+	if (*str == '\0')
 	{
 		return (len_str);
 	}
-	return (len_str);
+	else
+	{
+		len_str++;
+		return (len_str + _strlen_recursion(str + 1));
+	}
 }
