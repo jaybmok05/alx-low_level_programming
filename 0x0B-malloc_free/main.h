b66aa246f6804d3b7c1f9a78e17eb30bb913b685
jabulane <jabumokoena85@gmail.com>
2023-04-11 03:35:14 -0700

    adding directory and files

---
+++ 0x0B-malloc_free/main.h
@@ -0,0 +1,18 @@
+#ifndef MAIN_FILE_H
+#define MAIN_FILE_H
+
+/**
+ * _putchar - writes the character c to stdout
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ * @c: The character to print
+ */
+
+/*list of function prototypes*/
+int _putchar(char c);
+char *create_array(unsigned int size, char c);
+char *_strdup(char *str);
+char *str_concat(char *s1, char *s2);
+int **alloc_grid(int width, int height);
+void free_grid(int **grid, int height);
+#endif
