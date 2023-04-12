4f64cad151f48594807943798b299aef43f546d2
jabulane <jabumokoena85@gmail.com>
2023-04-12 15:18:00 -0700

    adding untracked

---
+++ 0-create_array.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * create_array - a function that creates an array of chars,
+ * and initializes it with a specific char.
+ * Return: Returns NULL if size = 0
+ * Returns a pointer to the array, or NULL if it fails
+ * @size: the byte to allocate
+ * @c: array of characters
+ */
+
+char *create_array(unsigned int size, char c)
+{
+}
