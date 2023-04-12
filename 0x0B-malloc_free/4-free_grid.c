982f72d417947756673054f0dba0ba0e2bc0cece
jabulane <jabumokoena85@gmail.com>
2023-04-12 15:05:24 -0700

    modifying 4-free_grid.c

--- 0x0B-malloc_free/4-free_grid.c
+++ 0x0B-malloc_free/4-free_grid.c
@@ -4,7 +4,7 @@
 /**
  * free_grid - a function that frees a 2 dimensional grid
  * previously created by your alloc_grid function.
- * @grid 1st parameter
+ * @grid: 1st parameter
  * @height: 2nd parameter
  */
 
