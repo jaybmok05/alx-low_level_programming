f1296e845e1dec85f664df7a4058f1a5182006b6
jabulane <jabumokoena85@gmail.com>
2023-04-13 01:40:44 -0700

    adding 101-strtow.c 100-argstostr.c

---
+++ 0x0B-malloc_free/100-argstostr.c
@@ -0,0 +1,49 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * argstostr - a function that concatenates all
+ * the arguments of your program.
+ * Return: returns NULL if ac == 0 or av == NULL,
+ * returns a pointer to a new string, or NULL if it fails
+ * @argc: count of arguments
+ * @argv: array of arguments 
+ */
+
+char *argstostr(int argc, char **argv)
+{
+	int index_0, index_1, size = 0;
+	char *av_ptr;
+
+	if (argc == 0 || argv == NULL)
+	{
+		return (NULL);
+	}
+
+	for (index_0 = 0; index_0 < argc; index_0++)
+	{
+		for (index_1 = 0; argv[index_0][index_1]; index_1++)
+		{
+			size++;
+		}
+		size++;
+	}
+	av_ptr = malloc(sizeof(char) * size + 1);
+
+	if (av_ptr == NULL)
+	{
+		return (NULL);
+	}
+	size = 0;
+
+	for (index_0 = 0; index_0 < argc; index_0++) 
+	{
+		for (index_1 = 0; argv[index_0][index_1]; index_1++)
+		{
+			av_ptr[size++] = argv[index_0][index_1];
+		}
+		av_ptr[size++] = '\n';
+	}
+	av_ptr[size] = '\0';
+	return (av_ptr);
+}
---
+++ 0x0B-malloc_free/101-strtow.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * strtow - a function that splits a string into words
+ * @str: string to split
+ * Return: returns a pointer to an array of strings
+ * returns NULL if str == NULL or str == ""
+ * If function fails, it should return NULL
+ */
+
+char **strtow(char *str)
+{
+}
