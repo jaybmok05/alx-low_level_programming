ee896d4aa5aabd5410a126f4fda5bd2ee07f5175
jabulane <jabumokoena85@gmail.com>
2023-04-14 03:53:44 -0700

    adding compile files

---
+++ 0x0C-more_malloc_free/main.h
@@ -0,0 +1,16 @@
+#ifndef MAIN_FILE_H
+#define MAIN_FILE_H
+/**
+ * malloc_checked - a function that allocates memory using malloc.
+ * Return: Returns a pointer to the allocated memory
+ * if malloc fails, the malloc_checked function should
+ * cause normal process termination with a status value of 98
+ * @bytes: parameter.
+ */
+
+/*list of function prototype*/
+void *malloc_checked(unsigned int b);
+char *string_nconcat(char *s1, char *s2, unsigned int n);
+void *_calloc(unsigned int nmemb, unsigned int size);
+int *array_range(int min, int max);
+#endif
