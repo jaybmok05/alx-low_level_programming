d180a52e4210480d23f1eff3dd4ef727a3d58d03
jabulane <jabumokoena85@gmail.com>
2023-04-17 08:14:42 -0700

    adding 0-object_like_macro.h

---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,14 @@
+#include "dog.h"
+#include <stdlib.h>
+
+/**
+ * new_dog -  a function that creates a new dog.
+ * @name: name of new dog
+ * @age: age of new dog
+ * @owner: owner of new dog
+ */
+
+dog_t *new_dog(char *name, float age, char *owner)
+{
+
+}
