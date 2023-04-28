c5a91ca8a158d6b077c513cbe0fad87b4583f355
jabulane <jabumokoena85@gmail.com>
2023-04-28 14:26:54 -0700

    adding 100-first.c

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,11 @@
+#include "lists.h"
+
+/**
+ * _print - a function prints before main
+ */
+
+void _print(void)
+{
+	printf("You're beat! and yet, you must allow,");
+	printf("\nI bore my house upon my back!\n");
+}
