896dd4e204ab2829d77ff8efd1c02531cbd6d390
jabulane <jabumokoena85@gmail.com>
2023-05-03 17:32:36 -0700

    modifying 7-get_nodeint.c

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -43,3 +43,22 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 		return (current);
 	}
 }
+
+/**
+ * listint_len - a function that returns the number
+ * of elements in a linked listint_t list.
+ * Return: returns the number of elements in a linked listint_t list
+ * @head: pointer to the first node
+ */
+
+size_t listint_len(const listint_t *head)
+{
+	size_t num_of_ele;
+
+	for (num_of_ele = 0; head != NULL; num_of_ele++)
+	{
+		head = head->next;
+	}
+
+	return (num_of_ele);
+}
