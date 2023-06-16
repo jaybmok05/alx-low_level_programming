78b8f96bb125dbdd01ea16b7e694949a13ec3004
jabulane <jabumokoena85@gmail.com>
2023-06-16 14:25:31 -0700

    fixing betty style error on line 18

--- 0x17-doubly_linked_lists/8-delete_dnodeint.c
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -15,7 +15,7 @@ int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 	unsigned int idx;
 
 	if (*head == NULL)
-        	return (-1);
+		return (-1);
 
 
 	for (idx = 0; curr != NULL; idx++, curr = curr->next)
