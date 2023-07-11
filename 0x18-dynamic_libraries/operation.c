cecc2033cec214dbada32191ae210e5f38482c2c
jabulane <jabumokoena85@gmail.com>
2023-07-11 06:04:38 -0700

    adding files

---
+++ 0x18-dynamic_libraries/operation.c
@@ -0,0 +1,54 @@
+#include <stdio.h>
+
+/**
+ * add - a function that sums values
+ * @a: first value
+ * @b: second value
+ * Return: the result
+ */
+
+int add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * sub - a function that subtract values
+ * @a: first value
+ * @b: second value
+ * Return: the result
+ */
+
+int sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * mul - a function that multiple values
+ * @a: first value
+ * @b: second value
+ * Return: the result
+ */
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * div - a function that divides values
+ * @a: first value
+ * @b: second value
+ * Return: the result
+ */
+
+int div(int a, int b)
+{
+	return (a / b);
+}
+
+int mod(int a, int b)
+{
+	return(a % b);
+}
